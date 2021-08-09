4778d5baad9e59ece6d349edcf87106ee568c75e
Nhan Do <trongnhan19081994@gmail.com>
2021-08-09 10:31:58 +0700

    login with JWT

--- api/index.js
+++ api/index.js
@@ -2,14 +2,20 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const authRoute = require('./routes/auth')
 
 dotenv.config()
 
+app.use(express.json())
+
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }).then(() => console.log('DB connection successfull!'))
     .catch((error) => console.log(error))
+
+app.use('/api/auth', authRoute)
 
 app.listen(8800, () => {
     console.log("Backend server is running!")
